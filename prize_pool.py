d60f170e6109b12c59ede307b4cde7f35b6ee19b
Sahil Dhanju <sddhanju@justin.tv>
2017-05-07 16:14:53 -0700

    Should have read the second stackoverflow answer

--- prize_pool.py
+++ prize_pool.py
@@ -8,8 +8,7 @@ def get_prize_pool():
     prize_pool_json = r.json()
     prize_pool = prize_pool_json["prize_pool"]
     
-    locale.setlocale(locale.LC_ALL, 'en_US')
-    prize_pool_amount = locale.format("%d", prize_pool, grouping=True)
+    prize_pool_amount = "{:,}".format(prize_pool)
     prize_pool_amount = "[$" + prize_pool_amount + "]"
 
     return prize_pool_amount
