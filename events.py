e2fb63bce23d8a8e87773cb44fbbd159474ae76b
Sahil Dhanju <sahildhanju1@gmail.com>
2017-09-17 22:11:07 -0500

    small fix

--- events.py
+++ events.py
@@ -34,7 +34,7 @@ def get_upcoming_events():
         endDate = datetime.strptime(event["end"]["date"], '%Y-%m-%d')
         endDate = endDate - timedelta(days=1)
         start = calendar.month_abbr[int(startDate.month)] + " " + str(int(startDate.day))
-        if datetime.now() > startDate.date():
+        if datetime.now() > startDate:
             start = "**Now!**"
         end = calendar.month_abbr[int(endDate.month)] + " " + str(int(endDate.day))
 
