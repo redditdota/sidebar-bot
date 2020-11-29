25f137ea7c8ebd76073d10cb8339a5385a962e76
Shuxin (Sushi) Zhan <shuxinzhan@gmail.com>
2020-11-28 23:24:27 -0500

    revert back to bc thread

--- battlecup.py
+++ battlecup.py
@@ -3,20 +3,36 @@ import praw
 import datetime
 import calendar
 
-posttitle = "Guild Recruitment Megathread"
+posttitle = "Find Your Battle Cup Party"
 
 postbody = """
+This thread is dedicated for you all to find a party for weekly Battle Cup.
+____
+**During the event all posts about finding/forming a team will be removed to avoid spam.**
+
+**This thread is only for people looking for a team, if you are forming a team simply reply to or PM anyone looking for a team that suits what you're looking for and invite them.**
+
+If you've already found a team please edit your post saying so or delete your post so no one will keep contacting you.
+
+##Sort by new to find people who just posted and is looking for a party.
+
+____
+
 Suggested Format:
 
-**Steam ID / Guild Name & Tag:**
+**Steam ID:**
 
 **Server:**
 
-**Language:**
+**Tier:** What tier do you want to play on?
 
-**Required Rank:**
+**Preferred Role** What positions you're comfortable playing with.
 
-**What makes your guild cool, unique, fun, etc:**
+**Other Information** Any other info that you would like your party mates to know
+
+---
+
+If you start to love playing with a premade and in a competitive setting please check out our friends at https://www.reddit.com/r/compDota2
 
 Have fun!
 
